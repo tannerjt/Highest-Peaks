c948d8d65e2c9f77c471c87ca331be967cb8448c
tannerjt <biolsurfer85@gmail.com>
2016-10-06 21:20:17 -0700

    remove tile, upgrade

---
+++ js/data/peaks.js
@@ -0,0 +1,52 @@
+define([], [
+	{
+		'name' : 'Mount McKinley',
+		'location' : [63.096635, -151.007021],
+		'height' : 20322
+	},
+	{
+		'name' : 'Mount Logan',
+		'location' : [60.566985, -140.405416],
+		'height' : 19551
+	},
+	{
+		'name' : 'Pico de Orizaba',
+		'location' : [19.029981, -97.268374],
+		'height' : 18491
+	},
+	{
+		'name' : 'Mount Saint Elias',
+		'location' : [60.290295, -140.930777],
+		'height' : 18009
+	},
+	{
+		'name' : 'Popocateptl',
+		'location' : [19.022756, -98.622976],
+		'height' : 17802
+	},
+	{
+		'name' : 'Mount Foraker',
+		'location' : [62.960382, -151.408264],
+		'height' : 17402
+	},
+	{
+		'name' : 'Mount Lucania',
+		'location' : [61.020884, -140.464249],
+		'height' : 17257
+	},
+	{
+		'name' : 'Iztaccihatl',
+		'location' : [19.178623, -98.642079],
+		'height' : 17159
+	},
+	{
+		'name' : 'King Peak',
+		'location' : [40.776392, -110.372741],
+		'height' : 16972
+	},
+	{
+		'name' : 'Mount Steele',
+		'location' : [61.091946, -140.306793],
+		'height' : 16644
+	}
+])
